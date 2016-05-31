db87201c8e09c50065d33f83964ffbd45c1c9507
Patrick Sattler <patrick.sattler@tum.de>
2016-05-31 10:45:11 +0200

    fix code_id_tuple creation now return the id of the enum

--- src/data_processing/util.py
+++ src/data_processing/util.py
@@ -319,21 +319,21 @@ class Location(GPSLocation):
         #     raise ValueError('id is not int')
         ret_list = []
         if self.city_name:
-            ret_list.append((self.city_name, (self.id, bytes(LocationCodeType.geonames.name, 'utf-8'))))
+            ret_list.append((self.city_name, (self.id, LocationCodeType.geonames.value)))
         for code in self.clli:
-            ret_list.append((code, (self.id, bytes(LocationCodeType.clli.name, 'utf-8'))))
+            ret_list.append((code, (self.id, LocationCodeType.clli.value)))
         for name in self.alternate_names:
-            ret_list.append((name, (self.id, bytes(LocationCodeType.geonames.name, 'utf-8'))))
+            ret_list.append((name, (self.id, LocationCodeType.geonames.value)))
         if self.locode:
             for code in self.locode.place_codes:
-                ret_list.append((code, (self.id, bytes(LocationCodeType.locode.name, 'utf-8'))))
+                ret_list.append((code, (self.id, LocationCodeType.locode.value)))
         if self.airport_info:
             for code in self.airport_info.iata_codes:
-                ret_list.append((code, (self.id, bytes(LocationCodeType.iata.name, 'utf-8'))))
+                ret_list.append((code, (self.id, LocationCodeType.iata.value)))
             for code in self.airport_info.icao_codes:
-                ret_list.append((code, (self.id, bytes(LocationCodeType.icao.name, 'utf-8'))))
+                ret_list.append((code, (self.id, LocationCodeType.icao.value)))
             for code in self.airport_info.faa_codes:
-                ret_list.append((code, (self.id, bytes(LocationCodeType.faa.name, 'utf-8'))))
+                ret_list.append((code, (self.id, LocationCodeType.faa.value)))
         return ret_list
 
     @staticmethod
