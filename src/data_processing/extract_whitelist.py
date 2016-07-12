388edb058fa10465d86995c9a348bf84d70b0167
Patrick Sattler <patrick.sattler@tum.de>
2016-07-12 23:13:08 +0200

    use trie for faster searching

--- src/data_processing/extract_whitelist.py
+++ src/data_processing/extract_whitelist.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import marisa_trie
 
 
 def main():
@@ -15,10 +16,11 @@ def main():
     with open(args.whitelist) as whitelist_file:
         for line in whitelist_file:
             whitelisted.append(line.strip())
+    whitelist_trie = marisa_trie.Trie(whitelisted)
     with open(args.filename) as sanitize_file, open(args.output_file, 'w') as output_file:
         for line in sanitize_file:
             line = line.strip().split(',')[0]
-            if line in whitelisted:
+            if line in whitelist_trie:
                 output_file.write(line)
 
 if __name__ == '__main__':
