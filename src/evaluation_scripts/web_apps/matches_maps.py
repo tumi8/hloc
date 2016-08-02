0e54408baed2330e0af3e9a17ea44f2e80e2e361
Patrick Sattler <patrick.sattler@tum.de>
2016-08-02 11:22:39 +0200

    simplify and improve web evaluation

--- src/evaluation_scripts/web_apps/matches_maps.py
+++ src/evaluation_scripts/web_apps/matches_maps.py
@@ -5,71 +5,74 @@ import src.data_processing.util as util
 import collections
 import heapq
 import numpy as np
+import sys
 
 
 RED_COLOR = '#FF0000'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', dest='drop_filename_proto', type=str,
+                        help=r'The path to the files with {} instead of the filenumber'
+                             ' in the name so it is possible to format the string')
+    parser.add_argument('-t', dest='trie_filename_proto', type=str,
+                        help=r'The path to the files with {} instead of the filenumber'
+                             ' in the name so it is possible to format the string')
+    parser.add_argument('-df', '--drop-file-count', type=int, default=8,
+                        dest='drop_file_count', help='number of files from preprocessing')
+    parser.add_argument('-tf', '--trie-file-count', type=int, default=8,
+                        dest='trie_file_count', help='number of files from preprocessing')
+    parser.add_argument('-loc', '--location-file-name', required=True, type=str,
+                        dest='location_filename', help='The path to the location file.'
+                        ' The output file from the codes_parser')
+    parser.add_argument('-a', action='store_true', dest='analyze')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('drop_filename_proto', type=str,
-                    help=r'The path to the files with {} instead of the filenumber'
-                         ' in the name so it is possible to format the string')
-parser.add_argument('-t', dest='trie_filename_proto', type=str,
-                    help=r'The path to the files with {} instead of the filenumber'
-                         ' in the name so it is possible to format the string')
-parser.add_argument('-f', '--drop-file-count', type=int, default=8,
-                    dest='drop_file_count', help='number of files from preprocessing')
-parser.add_argument('-tf', '--trie-file-count', type=int, default=8,
-                    dest='trie_file_count', help='number of files from preprocessing')
-parser.add_argument('-loc', '--location-file-name', required=True, type=str,
-                    dest='location_filename', help='The path to the location file.'
-                    ' The output file from the codes_parser')
-parser.add_argument('-a', action='store_true', dest='analyze')
+    args = parser.parse_args()
+    if not args.drop_filename_proto and not args.trie_filename_proto:
+        print('Neither drop filename proto nor trie filename proto is defined!', file=sys.stderr)
+        return 1
 
-args = parser.parse_args()
-location_counts = collections.defaultdict(int)
-codes = collections.defaultdict(int)
-for index in range(0, args.file_count):
-    with open(args.filename_proto.format(index)) as matches_file:
-        for line in matches_file:
-            domains = util.json_loads(line)
-            for domain in domains:
-                for match in domain.all_matches:
-                    location_counts[match.location_id] += 1
-                    codes[match.code] += 1
-
-with open(args.location_filename) as location_file:
-    locations = util.json_load(location_file)
-
-high_locs = heapq.nlargest(20, list(location_counts.items()), key=lambda x: x[1])
-print('most matched locations')
-for loc_id, count in high_locs:
-    location = locations[str(loc_id)]
-    print(location.city_name, location.lat, location.lon, count)
-
-high_codes = heapq.nlargest(20, list(codes.items()), key=lambda x: x[1])
-print('most matched location codes')
-for code, code_count in high_codes:
-    print(code, code_count)
+    with open(args.location_filename) as location_file:
+        locations = util.json_load(location_file)
+    drop_location_counts, trie_location_counts = [None] * 2
+    if args.drop_filename_proto:
+        drop_location_counts, drop_codes = get_codes_and_location_counts(
+            args.drop_filename_proto, args.drop_file_count)
+        print('### DROP Statistics ###')
+        calc_stats(drop_location_counts, drop_codes, locations, 'drop_codes.stats')
+    if args.trie_filename_proto:
+        trie_location_counts, trie_codes = get_codes_and_location_counts(
+            args.trie_filename_proto, args.trie_file_count)
+        print('### TRIE Statistics ###')
+        calc_stats(trie_location_counts, trie_codes, locations, 'trie_codes.stats')
 
 
 
-if not args.analyze:
-    application = flask.Flask(__name__, static_folder='/data/rdns-parse/src/evaluation_scripts/'
-                                                      'web_apps/static')
-    flask_googlemaps.GoogleMaps(application, key='AIzaSyBE3G8X89jm3rqBksk4OllYshmlUdYl1Ds')
+    if not args.analyze:
+        application = flask.Flask(__name__, static_folder='/data/rdns-parse/src/evaluation_scripts/'
+                                                          'web_apps/static')
+        flask_googlemaps.GoogleMaps(application, key='AIzaSyBE3G8X89jm3rqBksk4OllYshmlUdYl1Ds')
+
+        @application.route('/matches/<any(drop,trie):method>/<any(circles,markers):type>')
+        def matches_map(method, type):
+            location_counts = None
+            if method == 'drop':
+                location_counts = drop_location_counts
+            elif method == 'trie':
+                location_counts = trie_location_counts
+
+            if location_counts:
+                if type == 'circles':
+                    matches_map = create_matches_map_with_radius(location_counts, locations)
+                else:
+                    cluster = flask.request.args.get('cluster', None) is not None
+                    matches_map = create_matches_map_with_marker(location_counts, locations,
+                                                                 cluster)
+                return flask.render_template('matches_maps.html', matches_map=matches_map)
+
+        application.run()
 
 
-    @application.route('/matches/<any(drop,trie):method>/<any(circles,markers):type>')
-    def drop_matches_map(type):
-        if type == 'circles':
-            matches_map = create_matches_map_with_radius()
-        else:
-            cluster = flask.request.args.get('cluster', None) is not None
-            matches_map = create_matches_map_with_marker(cluster)
-        return flask.render_template('matches_maps.html', matches_map=matches_map)
-
-
-def create_matches_map_with_radius():
+def create_matches_map_with_radius(location_counts, locations):
     matches_map = flask_googlemaps.Map(identifier='matches_mao', lat=0, lng=0, zoom=4,
                                        maptype='TERRAIN', style='height:100%;')
     default_dict = {
@@ -87,7 +90,7 @@ def create_matches_map_with_radius():
     return matches_map
 
 
-def create_matches_map_with_marker(cluster: bool):
+def create_matches_map_with_marker(location_counts, locations, cluster: bool):
     matches_map = flask_googlemaps.Map(identifier='matches_mao', lat=0, lng=0, zoom=4,
                                        maptype='TERRAIN', style='height:100%;', cluster=cluster,
                                        cluster_imagepath='/static/images/m')
@@ -98,5 +101,36 @@ def create_matches_map_with_marker(cluster: bool):
     return matches_map
 
 
+def get_codes_and_location_counts(filename_proto, file_count):
+    location_counts = collections.defaultdict(int)
+    codes = collections.defaultdict(int)
+    for index in range(0, file_count):
+        with open(filename_proto.format(index)) as matches_file:
+            for line in matches_file:
+                domains = util.json_loads(line)
+                for domain in domains:
+                    for match in domain.all_matches:
+                        location_counts[match.location_id] += 1
+                        codes[match.code] += 1
+
+    return location_counts, codes
+
+
+def calc_stats(location_counts, codes, locations, output_filename):
+    high_locs = heapq.nlargest(20, list(location_counts.items()), key=lambda x: x[1])
+    print('most matched locations')
+    for loc_id, count in high_locs:
+        location = locations[str(loc_id)]
+        print(location.city_name, location.lat, location.lon, count)
+
+    high_codes = heapq.nlargest(20, list(codes.items()), key=lambda x: x[1])
+    print('most matched location codes')
+    for code, code_count in high_codes:
+        print(code, code_count)
+
+    codes_cdf = np.sort(codes.values())[::-1]
+    np.savetxt(output_filename, codes_cdf)
+
+
 if __name__ == "__main__":
-    application.run()
+    main()
