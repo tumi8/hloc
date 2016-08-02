52b650a3d765eeb334e980fabe502ad4e6b832d8
Patrick Sattler <patrick.sattler@tum.de>
2016-08-02 15:05:19 +0200

    add option to insert files from checking script

--- src/evaluation_scripts/web_apps/matches_maps.py
+++ src/evaluation_scripts/web_apps/matches_maps.py
@@ -5,7 +5,6 @@ import src.data_processing.util as util
 import collections
 import heapq
 import numpy as np
-import sys
 import logging
 import datetime
 
@@ -29,6 +28,8 @@ def main():
                         dest='location_filename', help='The path to the location file.'
                         ' The output file from the codes_parser')
     parser.add_argument('-a', action='store_true', dest='analyze')
+    parser.add_argument('-c', action='store_true', dest='checked',
+                        help='the input files are the output form checking dry run')
 
     args = parser.parse_args()
     if not args.drop_filename_proto and not args.trie_filename_proto:
@@ -42,12 +43,12 @@ def main():
     drop_location_counts, trie_location_counts = [None] * 2
     if args.drop_filename_proto:
         drop_location_counts, drop_codes = get_codes_and_location_counts(
-            args.drop_filename_proto, args.drop_file_count)
+            args.drop_filename_proto, args.drop_file_count, args.checked)
         logging.info('### DROP Statistics ###')
         calc_stats(drop_location_counts, drop_codes, locations, 'drop_codes.stats')
     if args.trie_filename_proto:
         trie_location_counts, trie_codes = get_codes_and_location_counts(
-            args.trie_filename_proto, args.trie_file_count)
+            args.trie_filename_proto, args.trie_file_count, args.checked)
         logging.info('### TRIE Statistics ###')
         calc_stats(trie_location_counts, trie_codes, locations, 'trie_codes.stats')
 
@@ -105,9 +106,11 @@ def create_matches_map_with_marker(location_counts, locations, cluster: bool):
     return matches_map
 
 
-def get_codes_and_location_counts(filename_proto, file_count):
+def get_codes_and_location_counts(filename_proto, file_count, checked):
     location_counts = collections.defaultdict(int)
     codes = {}
+    if checked:
+        return get_data_from_checked(filename_proto, file_count)
     for index in range(0, file_count):
         with open(filename_proto.format(index)) as matches_file:
             for line in matches_file:
@@ -119,6 +122,23 @@ def get_codes_and_location_counts(filename_proto, file_count):
                             codes[match.code] = collections.defaultdict(int)
                         codes[match.code]['count'] += 1
                         codes[match.code][str(match.location_id)] += 1
+
+    return location_counts, codes
+
+
+def get_data_from_checked(filename_proto, file_count):
+    location_counts = collections.defaultdict(int)
+    codes = {}
+    for index in range(0, file_count):
+        with open(filename_proto.format(index)) as matches_file:
+            for line in matches_file:
+                matches = util.json_loads(line)
+                for match in matches:
+                    location_counts[str(match.location_id)] += 1
+                    if match.code not in codes:
+                        codes[match.code] = collections.defaultdict(int)
+                    codes[match.code]['count'] += 1
+                    codes[match.code][str(match.location_id)] += 1
 
     return location_counts, codes
 
