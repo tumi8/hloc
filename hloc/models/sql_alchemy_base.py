77427c1af100c2a52be0e326eb646537dd142143
Patrick Sattler <sattler.patrick@posteo.de>
2017-04-04 12:31:52 +0200

    try to fix the base

--- hloc/models/sql_alchemy_base.py
+++ hloc/models/sql_alchemy_base.py
@@ -4,8 +4,13 @@
 import sqlalchemy.ext.declarative
 from hloc.util import engine
 
-from hloc.models import *
-
 
 Base = sqlalchemy.ext.declarative.declarative_base(bind=engine)
+
+
+from hloc.models import MeasurementResult, Probe, RipeAtlasProbe, AvailableType, JSONBase, \
+    LocationCodeType, AirportInfo, LocodeInfo, State, Location, LocationInfo, Domain, DomainLabel, \
+    CodeMatch, DRoPRule
+
+
 Base.metadata.create_all(engine)
