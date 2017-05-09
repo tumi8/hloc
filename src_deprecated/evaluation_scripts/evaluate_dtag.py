44f77ed0ac0316f5a0ebf7fdb62fba66929d04bd
Patrick Sattler <sattler.patrick@posteo.de>
2017-05-09 20:09:23 +0200

    improve dtag evaluation

--- src_deprecated/evaluation_scripts/evaluate_dtag.py
+++ src_deprecated/evaluation_scripts/evaluate_dtag.py
@@ -4,6 +4,7 @@ import argparse
 import socket
 import struct
 import collections
+import mmap
 
 import src_deprecated.data_processing.util as util
 
@@ -13,9 +14,14 @@ logger = None
 def __create_parser_arguments(parser):
     parser.add_argument('dtag_file', type=str,
                         help=r'The path to the dtag ground truth file')
-    parser.add_argument('ripe_filename_proto', type=str,
+    parser.add_argument('ripe_woip__filename_proto', type=str,
                         help=r'The path to the files with {} instead of the filenumber'
-                             ' in the name so it is possible to format the string')
+                             ' in the name so it is possible to format the string '
+                             '(without ip encoded)')
+    parser.add_argument('ripe_wip__filename_proto', type=str,
+                        help=r'The path to the files with {} instead of the filenumber'
+                             ' in the name so it is possible to format the string '
+                             '(with ip encoded)')
     parser.add_argument('-f', '--file-count', type=int, default=8,
                         dest='fileCount',
                         help='number of files from preprocessing')
@@ -49,32 +55,21 @@ def main():
     dtag_type_count = collections.defaultdict(int)
     dtag_domain_dict = collections.defaultdict(dict)
 
-    for i in range(0, args.fileCount):
-        with open(args.ripe_filename_proto.format(i)) as ripe_file:
-            for line in ripe_file:
-                domain_type_dict = util.json_loads(line)
-                for type, domains in domain_type_dict:
-                    for domain in domains:
-                        if domain.ip_address in dtag_ip_to_range:
-                            dtag_ip_count += 1
-                            dtag_type_count[type] += 1
-
-                            dtag_range = dtag_ip_to_range[domain.ip_address]
-
-                            if type not in dtag_domain_dict[dtag_range]:
-                                dtag_domain_dict[dtag_range][type] = []
-
-                            dtag_domain_dict[dtag_range][type].append(domain)
+    dtag_ip_count += parse_ripe_domains(args.ripe_woip__filename_proto, args.fileCount,
+                                        dtag_ip_to_range, dtag_type_count, dtag_domain_dict)
+    dtag_ip_count += parse_ripe_domains(args.ripe_wip__filename_proto, args.fileCount,
+                                        dtag_ip_to_range, dtag_type_count, dtag_domain_dict)
 
     with open(args.output_file, 'w') as output_file:
         util.json_dump(dtag_domain_dict, output_file)
 
-    logger.info('Collected {} dtag domains\nCorrect: {}, unknown: {}, no_location: {}, not_responding: {}'.format(
-        dtag_ip_count,
-        dtag_type_count[util.DomainType.correct.value],
-        dtag_type_count[util.DomainType.no_verification.value],
-        dtag_type_count[util.DomainType.no_location.value],
-        dtag_type_count[util.DomainType.not_responding.value]))
+    logger.info('Collected {} dtag domains\nCorrect: {}, unknown: {}, no_location: {}, '
+                'not_responding: {}'.format(
+                    dtag_ip_count,
+                    dtag_type_count[util.DomainType.correct.value],
+                    dtag_type_count[util.DomainType.no_verification.value],
+                    dtag_type_count[util.DomainType.no_location.value],
+                    dtag_type_count[util.DomainType.not_responding.value]))
 
 
 def parse_dtag_file(filepath):
@@ -87,19 +82,47 @@ def parse_dtag_file(filepath):
 
     return dtag_ip_to_location
 
+
 def dtag_ips_to_range(dtag_ip_to_location):
     dtag_ip_to_range = {}
     for ip_range in dtag_ip_to_location.keys():
         base_ip, subnet_bits = ip_range.split('/')
         base_ip_int = struct.unpack('>I', socket.inet_aton(base_ip))[0]
 
-        ips = [socket.inet_ntoa(struct.pack('>I', i)) for i in range(base_ip_int, base_ip_int + (2**int(subnet_bits)))]
+        ips = [socket.inet_ntoa(struct.pack('>I', i)) for i in range(base_ip_int, base_ip_int +
+                                                                     (2**int(subnet_bits)))]
 
         for ip in ips:
             dtag_ip_to_range[ip] = ip_range
 
     return dtag_ip_to_range
 
+
+def parse_ripe_domains(filename_proto, file_count, dtag_ip_to_range, dtag_type_count,
+                       dtag_domain_dict):
+    count = 0
+    for i in range(0, file_count):
+        with open(filename_proto.format(i)) as ripe_file_ptr, \
+                mmap.mmap(ripe_file_ptr.fileno(), 0, access=mmap.ACCESS_READ) as ripe_file:
+            line = ripe_file.readline().decode('utf-8')
+            while line:
+                domain_type_dict = util.json_loads(line)
+                for domain_type, domains in domain_type_dict:
+                    for domain in domains:
+                        if domain.ip_address in dtag_ip_to_range:
+                            count += 1
+                            dtag_type_count[domain_type] += 1
+
+                            dtag_range = dtag_ip_to_range[domain.ip_address]
+
+                            if domain_type not in dtag_domain_dict[dtag_range]:
+                                dtag_domain_dict[dtag_range][domain_type] = []
+
+                            dtag_domain_dict[dtag_range][domain_type].append(domain)
+
+                line = ripe_file.readline().decode('utf-8')
+
+    return count
 
 if __name__ == '__main__':
     main()
