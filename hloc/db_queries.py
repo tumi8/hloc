d57a6deb79f66a289c6fc5d7f909cc517a9c8979
Patrick Sattler <sattler.patrick@posteo.de>
2017-05-25 11:32:42 +0200

    rename queries file

--- hloc/db_queries.py
+++ hloc/db_queries.py
@@ -5,10 +5,10 @@ A collection of queries connected to the location object
 
 import sqlalchemy as sqla
 
-from hloc.models.location import State
+from hloc.models import State, Probe, Session
 
 
-def state_for_code(state_code, state_name, db_session):
+def state_for_code(state_code, state_name, db_session: Session):
     """
     :param state_code: A state code 
     :param state_name: the name of the state
@@ -26,3 +26,12 @@ def state_for_code(state_code, state_name, db_session):
     state = State(name=state_name, code=state_code)
     db_session.add(state)
     return State(name=state_name, code=state_code)
+
+
+def probe_for_id(probe_id: int, db_session: Session) -> Probe:
+    """
+    searches
+    :param probe_id (int): the id of the probe
+    :return (Probe): the Probe with the corresponding id or None
+    """
+    return db_session.query(Probe).filter(Probe.probe_id == probe_id).first()
