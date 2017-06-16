3bf8db7353df38eacaa11315b6908c6273ef2009
Patrick Sattler <sattler@users.noreply.github.com>
2017-06-16 23:32:59 +0200

    remove key

--- src/data_processing/util.py
+++ src/data_processing/util.py
@@ -19,7 +19,7 @@ DROP_RULE_TYPE_REGEX = re.compile(r'<<(?P<type>[a-z]*)>>')
 CLASS_IDENTIFIER = '_c'
 IPV4_IDENTIFIER = 'ipv4'
 IPV6_IDENTIFIER = 'ipv6'
-PROBE_API_KEY = '66bcd4c1-fdca-46f1-b1b9-8f7c333379e9'
+
 PROBE_API_URL_PING = \
     'https://kong.speedcheckerapi.com:8443/ProbeAPIService/Probes.svc/StartPingTestByBoundingBox'
 PROBE_API_URL_GET_PROBES = \
