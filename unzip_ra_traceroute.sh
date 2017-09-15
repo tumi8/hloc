b83ed93038d66cab6a68d535fc143e63c19bcf04
Patrick Sattler <sattler.patrick@posteo.de>
2017-09-15 14:21:16 +0200

    fix ping and traceroute unzipping

--- unzip_ra_traceroute.sh
+++ unzip_ra_traceroute.sh
@@ -5,5 +5,5 @@ oldestDateAllowed=`expr ${actualDate} - 24 \* 3600 \* 30`
 
 for filename in $@;
 do
-    bzcat ${filename} | jq -c '. | select(.timestamp >= ${oldestDateAllowed} and has("dst_addr")) | {timestamp: .timestamp, avg: .avg, dst_addr: .dst_addr, from: .from, min: .min, msm_id: .msm_id, type: .type, result: [.result[] | select(has("result")) | {result: [.result[] | select(has("rtt") and has("from") and has("err") == false) | {rtt: .rtt, ttl: .ttl, from: .from}], hop: .hop}], proto: .proto, src_addr: .src_addr, ttl: .ttl, prb_id: .prb_id}' > /data/ripe_atlas/${filename:22:`expr ${#filename} - 22 - 4`}
+    bzcat ${filename} | jq -c '. | select(.timestamp >= ${oldestDateAllowed} and has("dst_addr")) | {timestamp: .timestamp, avg: .avg, dst_addr: .dst_addr, from: .from, min: .min, msm_id: .msm_id, type: .type, result: [.result[] | select(has("result")) | {result: [.result[] | select(has("rtt") and has("from") and has("err") == false) | {rtt: .rtt, ttl: .ttl, from: .from}], hop: .hop}], proto: .proto, src_addr: .src_addr, ttl: .ttl, prb_id: .prb_id}' > /data/ripe-atlas/${filename:22:`expr ${#filename} - 22 - 4`}
 done
