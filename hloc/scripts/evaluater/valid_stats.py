c54350cc635c0563e57c122379ad706452442d9d
Patrick Sattler <sattler.patrick@posteo.de>
2017-09-26 14:46:07 +0200

    reverse the sorting of the evaluation

--- hloc/scripts/evaluater/valid_stats.py
+++ hloc/scripts/evaluater/valid_stats.py
@@ -88,11 +88,11 @@ def main():
         output_file.write(str_to_wrt)
 
     print('domains count: ')
-    pprint.pprint(sorted(domains_count.items(), key=operator.itemgetter(1)))
+    pprint.pprint(sorted(domains_count.items(), key=operator.itemgetter(1), reverse=True))
     print('location_id_count')
-    pprint.pprint(sorted(location_id_count.items(), key=operator.itemgetter(1)))
+    pprint.pprint(sorted(location_id_count.items(), key=operator.itemgetter(1), reverse=True))
     print('probes_count')
-    pprint.pprint(sorted(probes_count.items(), key=operator.itemgetter(1)))
+    pprint.pprint(sorted(probes_count.items(), key=operator.itemgetter(1), reverse=True))
 
 
 def domain_base_name(domain_name):
