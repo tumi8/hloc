1035db129f5006d4567f22807d371e643bab4f1e
Patrick Sattler <sattler.patrick@posteo.de>
2018-05-14 11:56:15 +0200

    block concurrent execution of cron-measurement-imports script

--- hloc/cron-measurement-imports.sh
+++ hloc/cron-measurement-imports.sh
@@ -11,35 +11,46 @@ nonDeletedPastDays=100
 # last 60 days will be imported
 importDays=5
 
+
 if [ -z $1 ] || [ -z $2 ] || [ ! -d $3 ]; then
     echo "provide path to hloc directory, the database name, and the python environment! Aborting!"
     exit 1
 else
-    source $3/bin/activate
+    (
+        if ! flock -e -n 200; then
+            echo "cron import already running not starting"
+            exit 0
+        fi
 
-    cd $1
+        source $3/bin/activate
 
-    logPath="logs"
-    if [ ! -d ${logPath} ]; then
-        mkdir ${logPath}
-    fi
+        cd $1
 
-    if [ ! -f "/var/cache/hloc/ripe_probes.cache" ]; then
-        if ! ./hloc/cache_ripe_probes.sh $1 $2 $3; then
+        logPath="logs"
+        if [ ! -d ${logPath} ]; then
+            mkdir ${logPath}
+        fi
+
+        if [ ! -f "/var/cache/hloc/ripe_probes.cache" ]; then
+            if ! ./hloc/cache_ripe_probes.sh $1 $2 $3; then
+                exit 1
+            fi
+        fi
+
+        python3 -m hloc.scripts.importer.delete_measurements --days-in-past ${nonDeletedPastDays} --database-name $2 -l ${logPath}/cron-delete-measurements.log -ll DEBUG &
+        if ! python3 -m hloc.scripts.importer.parse_ripe_archive ${archivepathripe} --number-processes 4 --days-in-past ${importDays} --database-name $2 -l ${logPath}/cron-ripe-import.log -ll DEBUG; then
+            echo "error while parsing ripe data"
             exit 1
         fi
-    fi
+        if ! python3 -m hloc.scripts.importer.parse_caida_archive ${archivepathcaidaipv4} --number-processes 4 --days-in-past ${importDays} --database-name $2 -l ${logPath}/cron-caida-import.log -ll DEBUG; then
+            echo "error while parsing caida v4 data"
+            exit 1
+        fi
+        if ! python3 -m hloc.scripts.importer.parse_caida_archive ${archivepathcaidaipv6} --number-processes 4 --days-in-past ${importDays} --database-name $2 -l ${logPath}/cron-caida-import.log -ll DEBUG; then
+            echo "error while parsing caida v6 data"
+            exit 1
+        fi
 
-    python3 -m hloc.scripts.importer.delete_measurements --days-in-past ${nonDeletedPastDays} --database-name $2 -l ${logPath}/cron-delete-measurements.log -ll DEBUG &
-    if ! python3 -m hloc.scripts.importer.parse_ripe_archive ${archivepathripe} --number-processes 4 --days-in-past ${importDays} --database-name $2 -l ${logPath}/cron-ripe-import.log -ll DEBUG; then
-        exit 1
-    fi
-    if ! python3 -m hloc.scripts.importer.parse_caida_archive ${archivepathcaidaipv4} --number-processes 4 --days-in-past ${importDays} --database-name $2 -l ${logPath}/cron-caida-import.log -ll DEBUG; then
-        exit 1
-    fi
-    if ! python3 -m hloc.scripts.importer.parse_caida_archive ${archivepathcaidaipv6} --number-processes 4 --days-in-past ${importDays} --database-name $2 -l ${logPath}/cron-caida-import.log -ll DEBUG; then
-        exit 1
-    fi
-
-    echo "import complete"
+        echo "import successfully completed"
+    ) 200>/tmp/hloc-import.lock
 fi
